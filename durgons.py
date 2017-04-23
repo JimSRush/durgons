cd34c4d95dccce3fd172a965f1885abe31d4739e
JimRush <jim.rush@homes.co.nz>
2017-04-23 21:01:13 +1200

    added comments

--- durgons.py
+++ durgons.py
@@ -1,23 +1,33 @@
 from random import randint
 
+#define some global variables
 dungeons = "Dungeons"
 an = "and"
 dragons = "Dragons"
 vowels = ['a', 'e', 'i', 'o', 'u']
 
+#Let's start by randoising the vowels in the strings.
 def replaceVowels(word):
-    for index, char in enumerate(word):
+    w = list(word)
+    for index, char in enumerate(w):
         if isVowel(char):
-            print char
-        #pick random vowel
-        #replace random vowel
-        #return word
-        # print s + '\n'
-        # print i
+            w[index] = randomiseVowel()
+    return ''.join(w)
 
+#Helper method
 def isVowel(char):
     if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
         return True
     return False
 
-replaceVowels(dungeons)
+def randomiseVowel():
+    return vowels[randint(0, len(vowels)-1)]
+
+
+def startItUp():
+    for x in range(0, 100):
+        print (replaceVowels(dungeons))
+        print (replaceVowels(an))
+        print (replaceVowels(dragons))
+
+startItUp()
